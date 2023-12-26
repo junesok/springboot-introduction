7f4938741f61dc479834e6dfa1a8a431401540d5
iloveglazeddonut <iloveglazeddonut@gmail.com>
2023-12-26 11:10:44 +0900

    정적 컨텐츠 -실시간으로 변경할 필요 없는 데이터 & 변화 없는 데이터 -동일한 모습을 모든 접속자들에게 보여줌
    MVC 템플릿엔진
    -localhost8080:/hello-mvc 요청
    -톰캣이 요청 받아 컨테이너로 반환
    -컨테이너가 컨트롤러에 hello-mvc 매핑된 @GetMapping()된 helloMvc() 찾음
    -Model에 키와 값 데이터를 내보내고 hello-template을 리턴
    -View에 해당하는 hello-template.html를 찾아주고 템플릿엔진(thymeleaf)를 연결 시켜주는 viewResolver가 동작
    -템플릿엔진(thymeleaf)은 html파일을 받아 View 템플릿을 변환 후 브라우저에게 넘김
    
    API 방식(객체 반환)
    -요청 받은 데이터를 JSON 형식으로 리턴

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -13,4 +15,43 @@ public class HelloController {
         return "hello";
     }
 
+    //MVC와 템플릿엔진
+    //템플릿엔진을 모델, 뷰, 컨트롤러 방식으로 쪼개서
+    //View 보여지는 화면, Controller와 Model 비즈니스 로직과 내부적인 처리
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    //API
+    //객체 반환
+    @GetMapping("hello-string")
+    @ResponseBody //http 통신프로토콜 body 부분에 이 데이터를 직접 넣겠다.
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+    //브라우저에서 템플릿엔진과 똑같이 보이지만, 페이지 소스를 보면 "hello " + 입력한 내용만 있음.
+    //이렇게 쓰는 경우는 별로 없음.
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+        //브라우저에서 보이는 내용 : {"name":"입력한 내용"}
+        //JSON 형식 - 키&값 쌍
+    }
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
