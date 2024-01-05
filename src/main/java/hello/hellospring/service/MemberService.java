7e2600e20e5ecf5dc2cbd39ad53ed3aeb7945ed6
iloveglazeddonut <iloveglazeddonut@gmail.com>
2024-01-05 17:28:21 +0900

    2024/01/05 컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,14 +3,18 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository; //= new MemberRepository() 생략;
 
     //같은 리포지토리를 사용하기 위해 추가 - 외부에서 데이터를 가져오게 하기 위해서
+    @Autowired
     public MemberService(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
